41f9f4a649b5345c087394ac7df9a2f52dda7c89
Lizkhen <slezadrakona@gmail.com>
2022-12-07 07:27:46 +0300

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите координаты x1, y1, z1 и x2, y2, z2");
+double ab = 0;
+double [] point = new double [6];
+for (int i = 0; i < point.Length; i++)
+    point [i] = Convert.ToDouble (Console.ReadLine());
+ab = Math.Sqrt ((point[3]-point[0])*(point[3]-point[0]) + (point[4]-point[1])*(point[4]-point[1]) + 
+(point[5]-point[2])*(point[5]-point[2]));
+Console.WriteLine (ab); 
