6889855a23d59d6a93d3e3284459a23770c7fde3
Lizkhen <slezadrakona@gmail.com>
2022-12-07 07:37:42 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите натуральное число");
+string? input = Console.ReadLine();
+if (int.TryParse(input, out int num))
+{
+    int number = Convert.ToInt32 (input);
+    if (number <= 0)
+    {
+        Console.WriteLine ("Нужно ввести НАТУРАЛЬНОЕ число!");
+    }
+    for (int i = 1; i <= number; i++)
+        Console.WriteLine (i*i*i);
+}
+else 
+    Console.WriteLine("Вы ввели не совсем число");
